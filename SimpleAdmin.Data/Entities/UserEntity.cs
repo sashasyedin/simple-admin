a75bffa39a6dcdbf1186c8f2420d07a2a5c3aad4
Alexander Ivanov <sashasyedin@outlook.com>
2018-11-22 17:06:52 +0200

    Add a database context

---
+++ SimpleAdmin.Data/Entities/UserEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleAdmin.Data.Entities
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public short Age { get; set; }
+    }
+}
---
+++ SimpleAdmin.Data/SimpleAdminContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAdmin.Data.Abstractions;
+using SimpleAdmin.Data.Entities;
+using SimpleAdmin.Data.Extensions;
+
+namespace SimpleAdmin.Data
+{
+    public class SimpleAdminContext : DbContext, IDbContext
+    {
+        public SimpleAdminContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.RemovePluralizingTableNameConvention();
+        }
+    }
+}
